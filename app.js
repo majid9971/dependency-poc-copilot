e8c4b64914fb822d86abced42c023bc937c4c1fa
Majid Siddiqui <97821088+majid9971@users.noreply.github.com>
2026-05-19 14:18:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello");
+const { exec } = require('child_process');
+
+app.get('/ping', (req, res) => {
+    exec("ping " + req.query.host);
+    res.send("Pinged");
 });
 
 app.listen(3000);
