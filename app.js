13f3b64935fbd31eaf651a9c0f15fe7d50f5c084
Majid Siddiqui <97821088+majid9971@users.noreply.github.com>
2026-05-19 11:59:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello");
+app.get('/user', (req, res) => {
+    const query = "SELECT * FROM users WHERE id = " + req.query.id;
+    res.send(query);
 });
 
 app.listen(3000);
